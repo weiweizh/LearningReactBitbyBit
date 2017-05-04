25fd6a48be18b9c8a542b13e7a3b679bca126568
Weiwei <weiweizh@kth.se>
2017-05-03 18:15:39 -0700

    Create index.js
    Index.js is to render the react App into a DOM element, in the example, it's the div with id 'root'.

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <App cat = {5} text = "this is the text defined in index" />,
+  document.getElementById('root')
+);
