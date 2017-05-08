06848926e5ef148f2a483ce5e56b69af8402c9eb
weiweizh <weiweizh@kth.se>
2017-05-07 23:04:37 -0700

    props validation method
    1 Use React.PropsType.isRequired
    
    2 Define your own validation function for specific needs. E.g minimum
    length.

---
+++ App-8propTypeValidation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// const App is stateless
+class App extends React.Component{
+  render(){
+    return <Title text="1122" />
+  }
+}
+
+const Title = (props) => <h1>Title: {props.text}</h1>
+
+Title.propTypes = {
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 6){
+      return new Error(`${propName} was too short`)
+    }
+  }
+}
+
+export default App
