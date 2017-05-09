c0f9cd89f26b1a6f4ea54d46f3b6068b6b8d8231
weiweizh <weiweizh@kth.se>
2017-05-09 00:30:16 -0700

    Set state in react life cycle
    1. You can set static state
    2. You can set intervals

---
+++ App-12SetStatesinLifeCycles.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+// const App is stateless
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {val: 0}
+    this.update = this.update.bind(this)
+  }
+
+  update(){
+    this.setState({val: this.state.val + 1})
+  }
+
+  componentWillMount(){
+    console.log('componentWillMount')
+    this.setState({m:2})
+  }
+  componentDidMount(){
+    console.log('componentDidMount')
+    this.inc = setInterval(this.update, 500)
+  }
+
+  render(){
+    console.log('render')
+    return <button onClick = {this.update}>{this.state.val * this.state.m}</button>
+  }
+
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
+    clearInterval(this.inc)
+  }
+}
+
+
+
+class Wrapper extends React.Component{
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render(){
+    return(
+      <div>
+        <button onClick = {this.mount.bind(this)}>Mount</button>
+        <button onClick = {this.unmount.bind(this)}>UnMount</button>
+        <div id='a'></div>
+      </div>
+      )
+  }
+}
+export default Wrapper
